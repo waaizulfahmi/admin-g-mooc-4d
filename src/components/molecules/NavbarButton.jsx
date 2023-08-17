8ca2e757efb678531d29e89cd8ce3c86eda02e13
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-08-17 21:08:59 +0700

    feat: add regis

--- src/components/molecules/NavbarButton.jsx
+++ src/components/molecules/NavbarButton.jsx
@@ -3,12 +3,25 @@ import PropTypes from 'prop-types';
 // third party
 import { useSession, signOut } from 'next-auth/react';
 
+// axios
+import { logoutApi } from '@/axios/auth';
+
 // component
 import BorderedButton from '../atoms/BorderedButton';
 import FillButton from '../atoms/FillButton';
 
 const NavbarButton = ({ className = 'gap-[33px]', btnBorderedText = 'Masuk', btnFillText = 'Daftar' }) => {
-    const { status } = useSession();
+    const { status, data } = useSession();
+    const token = data?.user?.token;
+
+    const handleSignOut = async () => {
+        if (token) {
+            const response = await logoutApi({ token });
+            if (response?.metadata?.status === 'success') {
+                signOut();
+            }
+        }
+    };
 
     switch (status) {
         case 'loading':
@@ -28,7 +41,7 @@ const NavbarButton = ({ className = 'gap-[33px]', btnBorderedText = 'Masuk', btn
                     <BorderedButton className='invisible px-[36px] py-[12px]' theme='dark'>
                         {btnBorderedText}
                     </BorderedButton>
-                    <FillButton onClick={() => signOut()} className='px-[36px] py-[12px]'>
+                    <FillButton onClick={handleSignOut} className='px-[36px] py-[12px]'>
                         Keluar
                     </FillButton>
                 </div>
