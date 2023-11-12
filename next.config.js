6a97d0815da47c4b87eae3437f5e8e4e70b0e810
waaizulfahmi <wandaaizulf@gmail.com>
2023-11-13 00:51:30 +0700

    update route to nurz.site

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'be.gmooc4.id'],
+        domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
     },
     eslint: {
         dirs: ['app', 'utils', 'components', 'redux'], // Only run ESLint on the [...] directories during production builds (next build)
--- src/axios/instance.js
+++ src/axios/instance.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const apiInstance = axios.create({
-    // baseURL: 'https://nurz.site/api',
-    baseURL: 'https://be.gmooc4d.id/api',
+    baseURL: 'https://nurz.site/api',
+    // baseURL: 'https://be.gmooc4d.id/api',
     withCredentials: true,
 });
 
 export const sanctumApiInstance = axios.create({
-    // baseURL: 'https://nurz.site/sanctum/csrf-cookie',
-    baseURL: 'https://be.gmooc4d.id/sanctum/csrf-cookie',
+    baseURL: 'https://nurz.site/sanctum/csrf-cookie',
+    // baseURL: 'https://be.gmooc4d.id/sanctum/csrf-cookie',
     withCredentials: true,
 });
