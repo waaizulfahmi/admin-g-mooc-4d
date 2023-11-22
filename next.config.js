632857a72ae0f85e3181f5f065d7cf1f4e062aa7
waaizulfahmi <wandaaizulf@gmail.com>
2023-11-22 21:46:48 +0700

    fs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
@@ -7,7 +8,7 @@ const nextConfig = {
         dirs: ['app', 'utils', 'components', 'redux'], // Only run ESLint on the [...] directories during production builds (next build)
     },
     reactStrictMode: false,
-    fs: 'empty',
+    fs: false,
 };
 
 module.exports = nextConfig;
